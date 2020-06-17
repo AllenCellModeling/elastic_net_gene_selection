e2eb52bdb2512593568ba2023c5aa3799bd6b7f6
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2020-06-17 13:50:16 -0700

    fix string

--- elastic_net_gene_selection/datasets/correlated_random_variables.py
+++ elastic_net_gene_selection/datasets/correlated_random_variables.py
@@ -14,10 +14,10 @@ def random_corr_mat(D=10, beta=1):
         for i in range(k + 1, D):
             P[k, i] = 2 * np.random.beta(beta, beta) - 1
             p = P[k, i]
-            for l in reversed(range(k)):
+            for ell in reversed(range(k)):
                 p = (
-                    p * np.sqrt((1 - P[l, i] ** 2) * (1 - P[l, k] ** 2))
-                    + P[l, i] * P[l, k]
+                    p * np.sqrt((1 - P[ell, i] ** 2) * (1 - P[ell, k] ** 2))
+                    + P[ell, i] * P[ell, k]
                 )
             S[k, i] = S[i, k] = p
 
@@ -30,7 +30,6 @@ def random_corr_mat(D=10, beta=1):
 
 
 def hub_spoke_corr_mat(D=50, groups=5, v=0.3, u=0.1):
-    """Port of data generation code from Wasserman's Huge package."""
     from statsmodels.stats.moment_helpers import cov2corr
 
     G = D // groups  # group size
